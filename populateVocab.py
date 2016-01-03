e132692fbdeecb0cdd3b99d8c5a2e56e14c1ad25
Rishabh Mehrotra <erishabh@gmail.com>
2016-01-02 17:45:45 -0700

    first commit...existing code

---
+++ populateVocab.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import sys,json
+import pickle
+
+
+print "hello"
+vocab = {"":0}
+i = 6
+while i>=0:
+	filestr = "data/"+str(i)+".allQueries.freq.nUsers.txt"
+	with open(filestr) as infile:
+		for line in infile:
+			line1 = line.split('\t')[0]
+			#print "line:_%s_"%line1
+			words = line1.split(' ')
+			for w in words:
+				if len(w)<2:
+					continue
+				#print w
+				if w in vocab:
+					c = vocab[w]
+					c+=1
+					vocab[w] = c
+				else:
+					#vocab.update({w: 1})
+					vocab[w] = 1
+	print "vocab size: %d"%len(vocab)
+	fileout = "data/"+str(i)+".vocab.txt"
+	oFile = open(fileout, 'w')
+	for (k,v) in vocab.items():
+		if len(k)>15:
+			continue
+		oFile.write(k+"\t"+str(vocab[k])+"\n")
+	oFile.close()
+	i-=1
+	vocab.clear()
